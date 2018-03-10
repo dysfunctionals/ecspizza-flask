a1f2090706451e21c7927a916bca01f5c8e96642
Dan Trickey <dan@trickey.io>
2018-03-10 23:03:21 +0000

    Add codes to restaurant page

--- app/views/restaurants.py
+++ app/views/restaurants.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify
 from flask_user import current_user, login_required, roles_accepted
 from flask import Blueprint, redirect, render_template
-from app.models.pizza_models import Restaurant
+from app.models.pizza_models import Restaurant, PromoCode
 
 # When using a Flask app factory we must use a blueprint to avoid needing 'app' for '@app.route'
 restaurants_blueprint = Blueprint('restaurants', __name__, template_folder='templates')
@@ -11,3 +11,10 @@ restaurants_blueprint = Blueprint('restaurants', __name__, template_folder='temp
 def index():
     restaurants = Restaurant.query.all()
     return render_template('pages/restaurants/index.html',restaurants=restaurants);
+
+@restaurants_blueprint.route('/<slug>')
+@login_required
+def view(slug):
+    restaurant = Restaurant.query.filter_by(slug=slug).first_or_404()
+    promo_count = PromoCode.query.filter_by(restaurant_id=restaurant.id).count()
+    return render_template('pages/restaurants/view.html', restaurant=restaurant, promo_count=promo_count);
