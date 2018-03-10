dee71bd5919fe74ebef44c2abb34129b305957aa
Dan Trickey <dan@trickey.io>
2018-03-10 15:33:10 +0000

    Removed profile page to do it later

--- app/views/misc_views.py
+++ app/views/misc_views.py
@@ -27,23 +27,7 @@ def admin_page():
     return render_template('pages/admin_page.html')
 
 
-@main_blueprint.route('/pages/profile', methods=['GET', 'POST'])
+@main_blueprint.route('/pages/profile', methods=['GET'])
 @login_required
 def user_profile_page():
-    # Initialize form
-    form = UserProfileForm(request.form, current_user)
-
-    # Process valid POST
-    if request.method == 'POST' and form.validate():
-        # Copy form fields to user_profile fields
-        form.populate_obj(current_user)
-
-        # Save user_profile
-        db.session.commit()
-
-        # Redirect to home page
-        return redirect(url_for('main.home'))
-
-    # Process GET or invalid POST
-    return render_template('pages/user_profile_page.html',
-                           form=form)
+    return render_template('pages/user_profile_page.html')
