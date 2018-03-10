1fd4b4f501a1f7f7caf21056eeb06deeebcd0260
Dan Trickey <dan@trickey.io>
2018-03-10 21:33:28 +0000

    Restaurants page now uses the db

--- app/views/restaurants.py
+++ app/views/restaurants.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify
 from flask_user import current_user, login_required, roles_accepted
 from flask import Blueprint, redirect, render_template
+from app.models.pizza_models import Restaurant
 
 # When using a Flask app factory we must use a blueprint to avoid needing 'app' for '@app.route'
 restaurants_blueprint = Blueprint('restaurants', __name__, template_folder='templates')
@@ -8,4 +9,5 @@ restaurants_blueprint = Blueprint('restaurants', __name__, template_folder='temp
 @restaurants_blueprint.route('/')
 @login_required
 def index():
-    return render_template('pages/restaurants/index.html');
+    restaurants = Restaurant.query.all()
+    return render_template('pages/restaurants/index.html',restaurants=restaurants);
