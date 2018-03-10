96f2fa60344681d8cf99d255b1b63e5f3d443eb8
Dan Trickey <dan@trickey.io>
2018-03-10 16:46:37 +0000

    Remove the default users and add a command to create them

--- README.md
+++ README.md
@@ -12,6 +12,10 @@ Based on [Flask-User-Starter-App](https://github.com/lingthio/Flask-User-starter
 - `pip install -r requirements.txt`
 - `cp app/local_settings_example.py app/local_settings.py`
 - `python manage.py init_db`
+- To add test users:
+    - `python manage.py add_users`
+    - Test User: member@example.com , Password1
+    - Test Admin: admin@example.com , Password1
 - Now run the dev server: `python manage.py runserver`
 
 ## Run the tests
--- manage.py
+++ manage.py
@@ -9,12 +9,13 @@ from flask_migrate import MigrateCommand
 from flask_script import Manager
 
 from app import create_app
-from app.commands import InitDbCommand
+from app.commands import *
 
 # Setup Flask-Script with command line commands
 manager = Manager(create_app)
 manager.add_command('db', MigrateCommand)
 manager.add_command('init_db', InitDbCommand)
+manager.add_command('add_users', AddUsersCommand)
 
 if __name__ == "__main__":
     # python manage.py                      # shows available commands
