82a31a06d7949aaa87397c9798e4ff78882d8a10
Dan Trickey <dan@trickey.io>
2018-03-11 09:56:30 +0000

    Tidy some stuff and make css nicer

--- app/views/pizza.py
+++ app/views/pizza.py
@@ -10,16 +10,7 @@ import datetime
 pizza_blueprint = Blueprint('pizza', __name__, template_folder='templates')
 
 
-@pizza_blueprint.route('/upload_test', methods=['GET', 'POST'])
-@login_required
-def upload_test():
-    if request.method == 'POST' and 'photo' in request.files:
-        filename = photos.save(request.files['photo'])
-        return filename
-    return render_template('pages/upload_test.html')
-
-
-@pizza_blueprint.route('/new', methods=['GET', 'POST'])
+@pizza_blueprint.route('/new', methods=['POST'])
 @login_required
 def upload():
     if request.method == 'POST' and 'photo' in request.files:
@@ -27,6 +18,3 @@ def upload():
         pizza_type = request.form['type']
         filename = photos.save(request.files['photo'], name=pizza_uuid + '.')
         return pizza_uuid
-    else:
-        pizza_types = PizzaType.query.order_by(PizzaType.name).all()
-        return render_template('pages/pizza_upload.html', pizza_types=pizza_types)
